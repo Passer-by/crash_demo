9f6d0c8f9fac14b1bc2c997dc84aae8ea0640ba1
友人A <58846244+a1017480401@users.noreply.github.com>
2022-09-23 14:22:25 +0800

    fix

--- android/app/src/main/kotlin/com/example/crash_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/crash_demo/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.crash_demo
 
 import android.content.Context
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        if (savedInstanceState != null) {
+//            MyApplication.flutterEngine.renderer.onTrimMemory(TRIM_MEMORY_COMPLETE)
+//        }
+    }
+
     override fun provideFlutterEngine(context: Context): FlutterEngine? {
         return MyApplication.flutterEngine
     }
